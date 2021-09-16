a8010ba867b8ca33ab3b79a09a25fcc07e825580
LulutasoAI <hinety3@gmail.com>
2021-09-17 02:10:01 +0900

    t-100~t-1 Auto correlation related Multi Linear Regression

--- TenAccorLR.py
+++ TenAccorLR.py
@@ -6,6 +6,8 @@ import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.linear_model import LinearRegression
 import math
+import pandas as np
+from datetime import datetime, date, timedelta
 
 Util = Baseutils()
 prices = Util.stksearch2p("TSLA",Util.start,Util.end)
@@ -13,16 +15,32 @@ df = Util.stksearch("TSLA",Util.start, Util.end)
 
 class LRshift10():
     def __init__(self):
+        self.name = "^N225"
         self.utility = Baseutils()
-        self.prices = Util.stksearch2p("TSLA",self.utility.start,self.utility.end)
-        self.df = Util.stksearch("TSLA",self.utility.start,self.utility.end)
+        self.prices = Util.stksearch2p(self.name,self.utility.start,self.utility.end)
+        self.df = Util.stksearch(self.name,self.utility.start,self.utility.end)
         print("latest data", self.df.iloc[-1:,0:0])
         self.n = 10
         self.scalar_ = StandardScaler()
+        self.scalar2 = StandardScaler()
+        self.lags = 75 #200 for NIkkei, 75 for tesla. Notsure.
 
-    def shifting(self):
-        df = self.df.copy()
-        n = self.n
+    def acorr_Close(self):
+        df = self.df
+        #print(df)
+        #df = df["Adj Close"]
+        newone = self.shifting(df,self.lags)
+        #print(newone)
+        newone = newone.drop(["High","Low","Open","Close","Volume"],axis = 1)
+        today = newone.iloc[-1:,:].drop(["next day"],axis=1).values
+        #print(today,"today")
+        newone = newone.dropna()
+        X_train, X_test, Y_train, Y_test, today = self.dataprocessing2(newone,today)
+        model = self.learning2(X_train, X_test, Y_train, Y_test)
+        predicted = model.predict(today)
+        return model, today, predicted
+
+    def shifting(self,df,n):
         for t in range(1,n):
             if t == 1:
                 a = df["Adj Close"].shift(+t).to_frame()
@@ -38,16 +56,37 @@ class LRshift10():
         a.columns = ["next day"]
         newone = pd.concat([newone,a],axis = 1)
         return newone
-
+    def learning2(self,X_train, X_test, Y_train, Y_test):
+        model= LinearRegression()
+        model.fit(X_train,Y_train)
+        r2_train = model.score(X_train,Y_train)
+        print(r2_train,"trainscore")
+        r2_test = model.score(X_test,Y_test)
+        print(r2_test,"testscore")
+        return model
+    def dataprocessing2(self,newone,today):
+        X = newone.drop(["next day"], axis=1)
+        Y = newone[["next day"]]
+        #X = self.scalar2.fit_transform(X)
+        #print(X.shape,"Xshape", ":::: lentoday",len(today))
+        #print(today)
+        #today = self.scalar2.transform(today)
+        tr_size = int(0.7*len(X))
+        X_train = X[0:tr_size]
+        X_test= X[tr_size:len(X)]
+        Y_train = Y.values[0:tr_size]
+        Y_test= Y.values[tr_size:len(X)]
+        std_reg = LinearRegression()
+        return X_train, X_test, Y_train, Y_test, today
 
     def dataprocessing_(self):
-        newone = self.shifting()
-        self.today = newone.iloc[-1:,:].drop(["next day"],axis=1).values
+        newone = self.shifting(self.df,self.n)
+        today = newone.iloc[-1:,:].drop(["next day"],axis=1).values
         newone = newone.dropna()
         X = newone.drop(["next day"], axis=1)
         Y = newone[["next day"]]
         X = self.scalar_.fit_transform(X)
-        self.today = self.scalar_.transform(self.today)
+        self.today = self.scalar_.transform(today)
         tr_size = int(0.7*len(X))
         X_train = X[0:tr_size]
         X_test= X[tr_size:len(X)]
@@ -73,7 +112,59 @@ class LRshift10():
         print(result)
         return result
 
+    def dataupdate(self,today,predicted):
+        temp_list = list(today[0])
+        temp_list = temp_list[1:]
+        temp_list.append(predicted)
+        output = np.array(temp_list).reshape(1,len(temp_list)).to_numpy()
+        return output
+
+    def visual_future_prediction(self):
+        #fixed
+        model, today, predicted = self.acorr_Close()
+        #arg = today, predicted, model
+        outputplot = list(self.prices)[-100:]
+        outputplot = list(reversed(outputplot))
+        date_today = datetime.today()
+        Y = []
+        daysback = 1
+        for i, a in enumerate(outputplot):
+            daysback -= 1
+            fixedday = date_today - timedelta(days=-daysback)
+            A = fixedday.strftime("%A")
+            while A == "Sunday" or A == "Saturday":
+                daysback -= 1
+                fixedday = date_today - timedelta(days=-daysback)
+                A = fixedday.strftime("%A")
+
+            Y.append(fixedday)
+        outputplot = list(reversed(outputplot))
+        Y = list(reversed(Y))
+        daysback = 0
+        for i in range(0,100):
+            daysback += 1
+            if i == 0:
+                new = self.dataupdate(today,predicted)
+            #today = LR.scalar2.transform(output)
+                predicted = model.predict(new)
+            else:
+                new = self.dataupdate(new,predicted)
+                predicted = model.predict(new)
+            outputplot.append(predicted)
+
+            fixedday = date_today + timedelta(days=daysback)
+            A = fixedday.strftime("%A")
+            while A == "Sunday" or A == "Saturday":
+                daysback += 1
+                fixedday = date_today + timedelta(days=daysback)
+                A = fixedday.strftime("%A")
+            Y.append(fixedday)
+        plt.plot(Y,outputplot)
+        plt.title("Future {} stock prices ".format(self.name))
+        plt.show()
+
 if __name__ == "__main__":
     LR = LRshift10()
     result = LR.estimation_dataprocessing_tomorrow()
-    #def estimation(self, x):
+    print("tomorrow's predicted price value of {}".format(LR.name),result)
+    LR.visual_future_prediction()
